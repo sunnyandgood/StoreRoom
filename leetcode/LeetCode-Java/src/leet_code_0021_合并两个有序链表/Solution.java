46638d3696ab56ca61d5a41ef464abbea2d2b0f9
sunnyandgood <wrhsunny@163.com>
2019-06-04 22:32:08 +0800

    21. 合并两个有序链表

---
+++ leetcode/LeetCode-Java/src/leet_code_0021_合并两个有序链表/Solution.java
@@ -0,0 +1,87 @@
+package leet_code_0021_合并两个有序链表;
+
+/**
+ * @Author: sunnyandgood
+ * @Date: 2019/6/4 22:20
+ * 21. 合并两个有序链表
+ */
+public class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        // 类似归并排序中的合并过程
+        ListNode dummyHead = new ListNode(0);
+        ListNode cur = dummyHead;
+
+        // 任一为空，直接连接另一条链表
+        if (l1 == null) {
+            cur.next = l2;
+        } else {
+            cur.next = l1;
+        }
+
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                cur.next = l1;
+                cur = cur.next;
+                l1 = l1.next;
+            } else {
+                cur.next = l2;
+                cur = cur.next;
+                l2 = l2.next;
+            }
+        }
+        return dummyHead.next;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        ListNode l1 = new ListNode(new int[]{1, 2, 4});
+        ListNode l2 = new ListNode(new int[]{1, 3, 4});
+        System.out.println(solution.mergeTwoLists(l1, l2));
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) {
+        val = x;
+    }
+
+    /**
+     * 链表构造函数，将 arr 转成链表，当前节点即为链表头节点
+     *
+     * @param arr
+     */
+    public ListNode(int[] arr) {
+        if (arr == null || arr.length == 0) {
+            throw new IllegalArgumentException("arr can not be empty!");
+        }
+        this.val = arr[0];
+        ListNode cur = this;
+        for (int i = 1; i < arr.length; i++) {
+            ListNode node = new ListNode(arr[i]);
+            cur.next = node;
+            cur = cur.next;
+        }
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder res = new StringBuilder();
+        ListNode cur = this;
+        while(cur != null) {
+            res.append(cur.val + "->");
+            cur = cur.next;
+        }
+        res.append("NULL");
+        return res.toString();
+    }
+}
+/**
+ * 将两个有序链表合并为一个新的有序链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。
+ *
+ * 示例：
+ *
+ * 输入：1->2->4, 1->3->4
+ * 输出：1->1->2->3->4->4
+ */
