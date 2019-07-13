69591d17a7fb1018a207b130154185442678433d
sunnyandgood <wrhsunny@163.com>
2019-07-13 23:00:33 +0800

    20、包含min函数的栈

---
+++ 剑指Offer/JianZhiOffer/src/JianZhiOffer_20_包含min函数的栈_举例让抽象具体化/MinInStack.java
@@ -0,0 +1,29 @@
+package JianZhiOffer_20_包含min函数的栈_举例让抽象具体化;
+
+/**
+ * @Author: sunnyandgood
+ * @Date: 2019/7/13 22:55
+ * 20、包含min函数的栈
+ * 题目描述：
+ * 定义栈的数据结构，请在该类型中实现一个能够得到栈中所含最小元素的min函数（时间复杂度应为O（1））。
+ *
+ * 解题思路：
+ *
+ */
+public class MinInStack {
+    public void push(int node) {
+
+    }
+
+    public void pop() {
+
+    }
+
+    public int top() {
+        return 0;
+    }
+
+    public int min() {
+        return 0;
+    }
+}
