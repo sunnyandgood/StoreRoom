041355a5c71883aff1ebc35f763501d13345e278
sunnyandgood <wrhsunny@163.com>
2019-07-22 23:07:46 +0800

    26、二叉搜索树与双向链表

---
+++ 剑指Offer/JianZhiOffer/src/JianZhiOffer_26_二叉搜索树与双向链表_分解让复杂问题简单/ConvertBinarySearchTree.java
@@ -0,0 +1,24 @@
+package JianZhiOffer_26_二叉搜索树与双向链表_分解让复杂问题简单;
+
+/**
+ * @Author: sunnyandgood
+ * @Date: 2019/7/22 23:05
+ * 26、二叉搜索树与双向链表
+ * 题目描述：
+ * 输入一棵二叉搜索树，将该二叉搜索树转换成一个排序的双向链表。要求不能创建任何新的结点，只能调整树中结点指针的指向。
+ */
+public class ConvertBinarySearchTree {
+    public TreeNode Convert(TreeNode pRootOfTree) {
+
+    }
+}
+class TreeNode {
+    int val = 0;
+    TreeNode left = null;
+    TreeNode right = null;
+
+    public TreeNode(int val) {
+        this.val = val;
+
+    }
+}
