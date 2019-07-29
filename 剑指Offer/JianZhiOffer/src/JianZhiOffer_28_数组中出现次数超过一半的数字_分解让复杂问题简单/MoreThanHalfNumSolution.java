6a4e12438e7d652b51c0559fce48718ab50eccba
sunnyandgood <wrhsunny@163.com>
2019-07-29 23:05:13 +0800

    public int MoreThanHalfNum_Solution(int [] array) {

---
+++ 剑指Offer/JianZhiOffer/src/JianZhiOffer_28_数组中出现次数超过一半的数字_分解让复杂问题简单/MoreThanHalfNumSolution.java
@@ -0,0 +1,15 @@
+package JianZhiOffer_28_数组中出现次数超过一半的数字_分解让复杂问题简单;
+
+/**
+ * @Author: sunnyandgood
+ * @Date: 2019/7/29 23:03
+ * 28、数组中出现次数超过一半的数字
+ * 题目描述：
+ * 数组中有一个数字出现的次数超过数组长度的一半，请找出这个数字。例如输入一个长度为9的数组{1,2,3,2,2,2,5,4,2}。
+ * 由于数字2在数组中出现了5次，超过数组长度的一半，因此输出2。如果不存在则输出0。
+ */
+public class MoreThanHalfNumSolution {
+    public int MoreThanHalfNum_Solution(int [] array) {
+
+    }
+}
