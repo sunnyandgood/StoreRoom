b7f35da5aa7fe19d0c5155a677dc3279ba774345
sunnyandgood <wrhsunny@163.com>
2019-07-30 23:45:24 +0800

    29、最小的个数

--- 剑指Offer/JianZhiOffer/src/JianZhiOffer_29_最小的个数_时间效率/GetLeastNumbers.java
+++ 剑指Offer/JianZhiOffer/src/JianZhiOffer_29_最小的个数_时间效率/GetLeastNumbers.java
@@ -1,6 +1,8 @@
 package JianZhiOffer_29_最小的个数_时间效率;
 
 import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.PriorityQueue;
 
 /**
  * @Author: sunnyandgood
@@ -11,6 +13,69 @@ import java.util.ArrayList;
  */
 public class GetLeastNumbers {
     public ArrayList<Integer> GetLeastNumbers_Solution(int [] input, int k) {
+        if (k == 0 || k > input.length) {
+            return new ArrayList<>();
+        }
+        // 先使用最大堆保存 k 个数，此后，每次和堆顶比较，若比堆顶小，移除堆顶，新数入堆
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(k, Comparator.reverseOrder());
+        for (int i = 0; i < input.length; i++) {
+            if (maxHeap.size() != k) {
+                maxHeap.add(input[i]);
+            } else if (input[i] <= maxHeap.peek()) {
+                maxHeap.remove();
+                maxHeap.add(input[i]);
+            }
+        }
+        return new ArrayList<>(maxHeap);
+    }
+//    public ArrayList<Integer> GetLeastNumbers_Solution_1(int [] input, int k) {
+//        ArrayList<Integer> list = new ArrayList<>();
+//        if (input == null || input.length == 0){
+//            return list;
+//        }
+//        int start = 0;
+//        int end = input.length - 1;
+//        int index = this.partition(input,start,end);
+//        while (index != k - 1){
+//            if (index > k - 1){
+//                end = index - 1;
+//                index = this.partition(input,start,end);
+//            }else {
+//                start = index + 1;
+//                index = this.partition(input,start,end);
+//            }
+//        }
+//        for (int i = 0;i < k;i++){
+//            list.add(input[i]);
+//        }
+//        return list;
+//    }
+//
+//    private int partition(int[] data,int start,int end){
+//        if (data == null || data.length == 0 || start < 0 || start >= end){
+//            return 0;
+//        }
+//        int index = new Random().nextInt(end - start) + start;
+//        this.swap(data,index,end);
+//        int small = start - 1;
+//        for (index = start;index < end;index++){
+//            if (data[index] < data[end]){
+//                this.swap(data,index,end);
+//            }
+//        }
+//        ++small;
+//        this.swap(data,small,end);
+//        return small;
+//    }
+//    private void swap(int[] data,int i,int j){
+//        int temp = data[i];
+//        data[i] = data[j];
+//        data[j] = temp;
+//    }
 
+    public static void main(String[] args) {
+        GetLeastNumbers numbers = new GetLeastNumbers();
+        System.out.println(numbers.GetLeastNumbers_Solution(new int[]{4,5,1,6,2,7,3,8},4));
+        System.out.println(numbers.GetLeastNumbers_Solution(new int[]{4,5,1,6,2,7,3,8},8));
     }
 }
