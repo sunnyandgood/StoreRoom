f390c9a9cc9ff4c6e510d00e255b8411e0289b14
sunnyandgood <wrhsunny@163.com>
2019-07-30 22:12:40 +0800

    29、最小的个数

---
+++ 剑指Offer/JianZhiOffer/src/JianZhiOffer_29_最小的个数_时间效率/GetLeastNumbers.java
@@ -0,0 +1,16 @@
+package JianZhiOffer_29_最小的个数_时间效率;
+
+import java.util.ArrayList;
+
+/**
+ * @Author: sunnyandgood
+ * @Date: 2019/7/30 21:56
+ * 29、最小的个数
+ * 题目描述：
+ * 输入n个整数，找出其中最小的K个数。例如输入4,5,1,6,2,7,3,8这8个数字，则最小的4个数字是1,2,3,4,。
+ */
+public class GetLeastNumbers {
+    public ArrayList<Integer> GetLeastNumbers_Solution(int [] input, int k) {
+
+    }
+}
