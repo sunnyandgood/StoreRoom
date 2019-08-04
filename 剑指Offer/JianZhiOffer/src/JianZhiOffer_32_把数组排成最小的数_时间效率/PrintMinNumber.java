fe4f635eb22baf48bd9eaeed61b90da58d16847a
sunnyandgood <wrhsunny@163.com>
2019-08-04 22:51:40 +0800

    32、把数组排成最小的数

--- 剑指Offer/JianZhiOffer/src/JianZhiOffer_32_把数组排成最小的数_时间效率/PrintMinNumber.java
+++ 剑指Offer/JianZhiOffer/src/JianZhiOffer_32_把数组排成最小的数_时间效率/PrintMinNumber.java
@@ -2,7 +2,6 @@ package JianZhiOffer_32_把数组排成最小的数_时间效率;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Comparator;
 
 /**
  * @Author: sunnyandgood
@@ -14,26 +13,25 @@ import java.util.Comparator;
  */
 public class PrintMinNumber {
     public String PrintMinNumber(int [] numbers) {
-        String s = "";
-        ArrayList<Integer> list = new ArrayList<Integer>();
+        if (numbers == null || numbers.length == 0){
+            return "";
+        }
+        StringBuffer buffer = new StringBuffer();
+        ArrayList<Integer> list = new ArrayList<>();
         for(int i = 0;i < numbers.length;i++){
             list.add(numbers[i]);//将数组放入arrayList中
         }
         //实现了Comparator接口的compare方法，将集合元素按照compare方法的规则进行排序
-        Collections.sort(list,new Comparator<Integer>(){
-            @Override
-            public int compare(Integer str1, Integer str2) {
-                // TODO Auto-generated method stub
-                String s1 = str1 + "" + str2;
-                String s2 = str2 + "" + str1;
+        Collections.sort(list, (Integer str1, Integer str2) -> {
+            String s1 = str1 + "" + str2;
+            String s2 = str2 + "" + str1;
 
-                return s1.compareTo(s2);
-            }
+            return s1.compareTo(s2);
         });
 
-        for(int j:list){
-            s += j;
+        for(int val : list){
+            buffer.append(val);
         }
-        return s;
+        return buffer.toString();
     }
 }
