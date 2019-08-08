c0ef221d9e814ba7247bbefe464ef9c159a5ab30
sunnyandgood <wrhsunny@163.com>
2019-08-08 23:28:17 +0800

    36、两个链表的第一个公共结点

---
+++ 剑指Offer/JianZhiOffer/src/JianZhiOffer_36_两个链表的第一个公共结点_时间空间效率的平衡/FindFirstCommonNode.java
@@ -0,0 +1,22 @@
+package JianZhiOffer_36_两个链表的第一个公共结点_时间空间效率的平衡;
+
+/**
+ * @Author: sunnyandgood
+ * @Date: 2019/8/8 23:19
+ * 36、两个链表的第一个公共结点
+ * 题目描述
+ * 输入两个链表，找出它们的第一个公共结点。
+ */
+public class FindFirstCommonNode {
+    public ListNode FindFirstCommonNode(ListNode pHead1, ListNode pHead2) {
+
+    }
+}
+class ListNode {
+    int val;
+    ListNode next = null;
+
+    ListNode(int val) {
+        this.val = val;
+    }
+}
