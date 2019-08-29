329f0120d260a004f6449d1c8a26182f90a43973
sunnyandgood <wrhsunny@163.com>
2019-08-30 00:08:44 +0800

    49、把字符串转换成整数

--- 剑指Offer/JianZhiOffer/src/JianZhiOffer_49_把字符串转换成整数_综合/StringToInt.java
+++ 剑指Offer/JianZhiOffer/src/JianZhiOffer_49_把字符串转换成整数_综合/StringToInt.java
@@ -23,6 +23,31 @@ package JianZhiOffer_49_把字符串转换成整数_综合;
  */
 public class StringToInt {
     public int StrToInt(String str) {
-
+        if (str == null || str.length() == 0) {
+            return 0;
+        }
+        // 判断正负性(1为正，-1为负)
+        int flag = 1;
+        char[] chArr = str.toCharArray();
+        int i = 0;
+        if (chArr[i] == '-') {
+            flag = -1;
+            i++;
+        } else if (chArr[i] == '+') {
+            i++;
+        }
+        // 转换
+        long sum = 0;
+        while (i < chArr.length) {
+            if (chArr[i] < '0' || chArr[i] > '9') {
+                return 0;
+            }
+            sum = sum * 10 + flag * (chArr[i++] - '0');
+            // 判断是否小于负整数最小值或大于正整数最大值
+            if (sum < Integer.MIN_VALUE || sum > Integer.MAX_VALUE) {
+                return 0;
+            }
+        }
+        return (int) sum;
     }
 }
