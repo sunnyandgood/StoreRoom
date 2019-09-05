6792a07c16b23c53259ced460dcdd7c0e873a542
sunnyandgood <wrhsunny@163.com>
2019-09-05 23:30:07 +0800

    55、链表中环的入口结点

---
+++ 剑指Offer/JianZhiOffer/src/JianZhiOffer_55_链表中环的入口结点_链表/EntryNodeOfLoop.java
@@ -0,0 +1,22 @@
+package JianZhiOffer_55_链表中环的入口结点_链表;
+
+/**
+ * @Author: sunnyandgood
+ * @Date: 2019/9/5 23:28
+ * 55、链表中环的入口结点
+ * 题目描述：
+ * 给一个链表，若其中包含环，请找出该链表的环的入口结点，否则，输出null。
+ */
+public class EntryNodeOfLoop {
+    public ListNode EntryNodeOfLoop(ListNode pHead){
+
+    }
+}
+class ListNode {
+    int val;
+    ListNode next = null;
+
+    ListNode(int val) {
+        this.val = val;
+    }
+}
