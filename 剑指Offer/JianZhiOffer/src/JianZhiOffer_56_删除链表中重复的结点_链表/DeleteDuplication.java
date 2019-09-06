729b60b5a539a0453cee16956e8868bf4a1f0b93
sunnyandgood <wrhsunny@163.com>
2019-09-06 23:29:02 +0800

    56、删除链表中重复的结点

---
+++ 剑指Offer/JianZhiOffer/src/JianZhiOffer_56_删除链表中重复的结点_链表/DeleteDuplication.java
@@ -0,0 +1,23 @@
+package JianZhiOffer_56_删除链表中重复的结点_链表;
+
+/**
+ * @Author: sunnyandgood
+ * @Date: 2019/9/6 23:27
+ * 56、删除链表中重复的结点
+ * 题目描述：
+ * 在一个排序的链表中，存在重复的结点，请删除该链表中重复的结点，重复的结点不保留，返回链表头指针。
+ * 例如，链表1->2->3->3->4->4->5 处理后为 1->2->5
+ */
+public class DeleteDuplication {
+    public ListNode deleteDuplication(ListNode pHead){
+
+    }
+}
+class ListNode {
+    int val;
+    ListNode next = null;
+
+    ListNode(int val) {
+        this.val = val;
+    }
+}
